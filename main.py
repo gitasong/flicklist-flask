7a1c2d069a439915b6ef4654a34e8e3d81162701
Nicole Freed <nicole.freed@gmail.com>
2017-06-17 18:12:54 -0700

    create section for tomorrow's movie; create random function for both today's and tomorrow's movies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    tomorrows_movie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
@@ -17,13 +19,20 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrows_movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie_list = ["Romeo and Juliet", "The Unbearable Lightness of Being", "Entre Nous", "The Piano", "Inception"]
+    # return random.choice(movie_list)
+    idx = random.randrange(len(movie_list))
+    return movie_list[idx]
 
 
 app.run()
